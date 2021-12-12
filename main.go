67cf4cc1e2caf11ba9e3aa4fb0b710a8f80e97f9
007hacky007 <007hacky007@users.noreply.github.com>
2021-12-12 18:47:08 +0100

    Async device scanning

--- main.go
+++ main.go
@@ -3,71 +3,162 @@ package main
 import (
 	"flag"
 	"fmt"
+	cmap "github.com/orcaman/concurrent-map"
+	"io"
 	"log"
 	"net/http"
 	"os/exec"
 	"regexp"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/vharitonsky/iniflags"
 )
 
 var (
-	portPtr = flag.String("port", "8081", "Port to listen on")
-	re      = regexp.MustCompile(`(?m)^(\S{2}:){5}\S{2}$`)
+	portPtr             = flag.String("port", "8081", "Port to listen on")
+	macPtr              = flag.String("mac", "", "Bluetooth MAC addresses separated with comma")
+	intervalPtr         = flag.Int("scan-sleep", 60, "Check bluetooth devices every X seconds")
+	pingCountPtr        = flag.Int("ping-count", 2, "Number of pings per check")
+	oldDataThresholdPtr = flag.Int64("old-data-threshold", 120, "Old data threshold in seconds")
+	re                  = regexp.MustCompile(`(?m)^(\S{2}:){5}\S{2}$`)
+	devices             cmap.ConcurrentMap
 )
 
+type device struct {
+	mac         string
+	resultJson  string
+	lastChecked time.Time
+}
+
+func init() {
+	devices = cmap.New()
+}
+
 func main() {
+	iniflags.Parse()
+	macAddresses := getMacAddresses()
+	quit := make(chan bool)
+	go btChecker(macAddresses, quit)
 	http.HandleFunc("/", handler)
 	log.Println("Listing on port: " + string(*portPtr))
 	err := http.ListenAndServe(":"+*portPtr, nil)
 	if err != nil {
-		log.Fatalln("Can not listen: " + err.Error())
+		log.Fatalln("Can not listen:", err.Error())
 		return
+	}
+	quit <- true
+}
+
+func saveBluetoothDevice(mac string, exitCode int) {
+	lastChecked := time.Now()
+	resultJson := ""
+	if exitCode == 0 {
+		resultJson = getJson(1, "in range")
+	} else {
+		resultJson = getJson(0, "not found")
+	}
+	devices.Set(mac, device{mac: mac, resultJson: resultJson, lastChecked: lastChecked})
+}
+
+func getMac(mac string) string {
+	if devices.Has(mac) == false {
+		return getJson(0, "Unknown MAC")
+	}
+	d, _ := devices.Get(mac)
+	device, _ := d.(device)
+	if time.Now().Unix()-device.lastChecked.Unix() > *oldDataThresholdPtr {
+		return getJson(0, "Last checked is too old "+strconv.FormatInt(time.Now().Unix()-device.lastChecked.Unix(), 10))
+	}
+
+	return device.resultJson
+}
+
+func checkDevice(mac string) int {
+	cmd := exec.Command("/usr/bin/l2ping",
+		"-t 1",
+		"-c "+strconv.Itoa(*pingCountPtr),
+		mac)
+	exitCode := "0"
+	out, err := cmd.Output()
+	if werr, ok := err.(*exec.ExitError); ok {
+		exitCode = werr.Error()
+	}
+	log.Printf("mac: %s | cmd exit code: %s | cmd output: %s\n", mac, exitCode, out)
+
+	exitCodeInt, err := strconv.Atoi(exitCode)
+	if err != nil {
+		return -99
+	}
+
+	return exitCodeInt
+}
+
+func btChecker(macAddresses []string, done chan bool) {
+	checkDevices(macAddresses)
+	ticker := time.NewTicker(time.Duration(*intervalPtr) * time.Second)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-done:
+			return
+		case t := <-ticker.C:
+			fmt.Println("Tick at", t)
+			checkDevices(macAddresses)
+		}
 	}
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func checkDevices(macAddresses []string) {
+	for _, mac := range macAddresses {
+		exitCode := checkDevice(mac)
+		saveBluetoothDevice(mac, exitCode)
+	}
+}
 
+func getMacAddresses() []string {
+	macAddresses := strings.Split(*macPtr, ",")
+	if len(macAddresses) < 1 {
+		log.Fatalln("No Bluetooth MAC addresses has been defined (use --mac parameter)")
+	}
+
+	for _, mac := range macAddresses {
+		if re.MatchString(mac) == false {
+			log.Fatalln(mac, "is not valid MAC address.")
+		}
+	}
+
+	return macAddresses
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
 	keys, ok := r.URL.Query()["mac"]
 
 	if !ok || len(keys[0]) < 1 {
 		log.Println("Url Param 'mac' is missing")
-		_, err := fmt.Fprintf(w, "Missing 'mac' parameter")
-		if err != nil {
-			return
-		}
+		httpResponse(w, "Missing 'mac' parameter", "")
 		return
 	}
 
-	// Query()["key"] will return an array of items,
-	// we only want the single item.
 	key := keys[0]
-
-	if re.MatchString(string(key)) {
-		cmd := exec.Command("/usr/bin/l2ping",
-			"-t 1",
-			"-c 2",
-			key)
-		exitCode := "0"
-		out, err := cmd.Output()
-		if werr, ok := err.(*exec.ExitError); ok {
-			exitCode = werr.Error()
-		}
-		log.Printf("mac: %s | cmd exit code: %s | cmd output: %s\n", key, exitCode, out)
-
-		if exitCode == "0" {
-			httpResponse(w, 1, "in range")
-		} else {
-			httpResponse(w, 0, "not found")
-		}
+	if re.MatchString(key) {
+		httpResponse(w, getMac(key), key)
 	} else {
-		log.Printf("Wrong input\n")
-		httpResponse(w, 0, "wrong input")
+		log.Println("Wrong input: " + key)
+		httpResponse(w, getJson(0, "wrong input"), key)
 	}
 }
 
-func httpResponse(w http.ResponseWriter, result int, description string) {
-	log.Printf("{\"description\":\"%s\",\"state\":%d}", description, result)
-	_, err := fmt.Fprintf(w, "{\"description\":\"%s\",\"state\":%d}", description, result)
+func httpResponse(w io.Writer, response string, mac string) {
+	log.Printf("[%s]: %s", mac, response)
+	_, err := fmt.Fprintf(w, response)
 	if err != nil {
-		log.Println(err)
+		log.Println("Could not write HTTP response:", err.Error())
+		return
 	}
+}
+
+func getJson(result int, description string) string {
+	return fmt.Sprintf("{\"description\":\"%s\",\"state\":%d}", description, result)
 }
